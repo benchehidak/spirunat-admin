d77f2d71a5fb665f50901de19b84a6448fffb4b8
Khaled Ben chehida <khaledbenchehida@192.168.1.76>
2025-02-06 19:19:07 +0100

    refactor GroupChart2 component to use props for statistics and comment out unused chart configurations

--- app/admin/(dashboard)/page.jsx
+++ app/admin/(dashboard)/page.jsx
@@ -1,123 +1,214 @@
 "use client";
-
-import React, { useState } from "react";
-import Card from "@/components/ui/Card";
-import ImageBlock2 from "@/components/partials/widget/block/image-block-2";
+import { useSession } from "next-auth/react";
+import { React, useEffect, useState } from "react";
 import GroupChart2 from "@/components/partials/widget/chart/group-chart-2";
-import RevenueBarChart from "@/components/partials/widget/chart/revenue-bar-chart";
-import ProfitChart from "@/components/partials/widget/chart/profit-chart";
-import OrderChart from "@/components/partials/widget/chart/order-chart";
-import EarningChart from "@/components/partials/widget/chart/earning-chart";
-import SelectMonth from "@/components/partials/SelectMonth";
-import Customer from "@/components/partials/widget/customer";
-import RecentOrderTable from "@/components/partials/table/recentOrder-table";
-import BasicArea from "@/components/partials/chart/appex-chart/BasicArea";
-import VisitorRadar from "@/components/partials/widget/chart/visitor-radar";
-import MostSales2 from "@/components/partials/widget/most-sales2";
-import Products from "@/components/partials/widget/products";
-import HomeBredCurbs from "@/components/partials/HomeBredCurbs";
-import { redirect } from "next/navigation";
+import Loading from "@/components/Loading";
+import Card from "@/components/ui/Card";
+// import { useSession } from "next-auth/react";
 
+const StarterPage = () => {
+  const { data: session } = useSession();
+  // console.log(session);
+  const [data, setData] = useState([]);
+  const [bookings, setBookings] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  // const { data: session } = useSession();
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await fetch("/api/protected/buyers", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({}),
+        });
+        const json = await res.json();
+        setData(json);
+      } catch (error) {
+        setError(error);
+      } finally {
+        try {
+          const res = await fetch("/api/protected/bookings", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({}),
+          });
+          const json = await res.json();
+          setBookings(json);
+        } catch (error) {
+          setError(error);
+        } finally {
+          setLoading(false);
+        }
+      }
+    };
+    fetchData();
+  }, []);
+  //remove tickets with price 0
+  const filteredData = data.filter(ticket => ticket.price !== 0);
 
-const Ecommerce = () => {
-  const [filterMap, setFilterMap] = useState("usa");
-  redirect("/admin/orders");
+  const totalTickets = filteredData.length;
+  const totalPrice = data.reduce((sum, ticket) => sum + ticket.price, 0);
+  const totalBookings = bookings.length;
+
+  const sellers = {};
+  let mostActiveSeller = null;
+  let maxTicketsSold = 0;
+
+  let earlybird = 0;
+  let firstrelease = 0;
+  let secondrelease = 0;
+  let scannedtickets = 0;
+
+  data.forEach((ticket) => {
+    if (!(ticket.price === 0)) {
+      const { seller, price } = ticket;
+      if (!sellers[seller]) {
+        sellers[seller] = 1;
+      } else {
+        sellers[seller]++;
+      }
+
+      if (sellers[seller] > maxTicketsSold) {
+        maxTicketsSold = sellers[seller];
+        mostActiveSeller = seller;
+      }
+
+      if (price === 10) {
+        earlybird++;
+      } else if (price === 40) {
+        secondrelease++;
+      }
+      if (ticket.status === "scanned") {
+        scannedtickets++;
+      }
+      
+    }
+  });
+  console.log(sellers);
+  const rows = Object.entries(sellers).map(([name, tickets]) => ({
+    name,
+    tickets,
+  }));
+  const columns = [
+    {
+      label: "Name",
+      field: "name",
+    },
+
+    {
+      label: "Tickets",
+      field: "tickets",
+    },
+  ];
+
+  const statistics = [
+    {
+      title: "Bookings",
+      count: totalBookings,
+      bg: "bg-[#E5F9FF] dark:bg-slate-900	",
+      text: "text-info-500",
+      icon: "heroicons:calendar",
+    },
+    {
+      title: "Sold Tickets",
+      count: totalTickets,
+      bg: "bg-[#FFEDE6] dark:bg-slate-900	",
+      text: "text-warning-500",
+      icon: "heroicons:ticket",
+    },
+    {
+      title: "Total Revenue",
+      count: `${totalPrice} DT`,
+      bg: "bg-[#d4fff3] dark:bg-slate-900	",
+      text: "text-[#4E8074]",
+      icon: "heroicons:banknotes",
+    },
+
+    {
+      title: "Most Active Seller",
+      count: mostActiveSeller,
+      bg: "bg-[#EAE6FF] dark:bg-slate-900	",
+      text: "text-[#5743BE]",
+      icon: "heroicons:user-group",
+    },
+    {
+      title: "Early bird",
+      count: earlybird,
+      bg: "bg-[#EAE6FF] dark:bg-slate-900	",
+      text: "text-[#5743BE]",
+      icon: "heroicons:arrow-trending-up-solid",
+    },
+    {
+      title: "First release",
+      count: firstrelease,
+      bg: "bg-[#EAE6FF] dark:bg-slate-900	",
+      text: "text-[#5743BE]",
+      icon: "heroicons:arrow-trending-up-solid",
+    },
+    {
+      title: "Second release",
+      count: secondrelease,
+      bg: "bg-[#EAE6FF] dark:bg-slate-900	",
+      text: "text-[#5743BE]",
+      icon: "heroicons:arrow-trending-up-solid",
+    },
+    {
+      title: "Scanned tickets",
+      count: scannedtickets,
+      bg: "bg-[#EAE6FF] dark:bg-slate-900	",
+      text: "text-[#5743BE]",
+      icon: "heroicons:qr-code",
+    },
+  ];
+
+  // if (loading) return <Loading />;
+  // if (error) return "Error!";
+  console.log(data);
+  console.log(bookings);
+
   return (
-    <div>
-      <h1>HELLOOO HERE </h1>
-      {/* <HomeBredCurbs title="Ecommerce" />
-      <div className="grid grid-cols-12 gap-5 mb-5">
-        <div className="2xl:col-span-3 lg:col-span-4 col-span-12">
-          <ImageBlock2 />
-        </div>
+    <>
+      <div className="grid grid-cols-full gap-5 mb-5">
         <div className="2xl:col-span-9 lg:col-span-8 col-span-12">
-          <div className="grid md:grid-cols-3 grid-cols-1 gap-4">
-            <GroupChart2 />
+          <div className="grid md:grid-cols-4 grid-cols-1 gap-4">
+            <GroupChart2 statistics={statistics} />
           </div>
         </div>
       </div>
-      <div className="grid grid-cols-12 gap-5">
-        <div className="2xl:col-span-8 lg:col-span-7 col-span-12">
-          <Card>
-            <div className="legend-ring">
-              <RevenueBarChart height={420} />
+      <Card title="Sellers" noborder>
+        <div className="overflow-x-auto -mx-6">
+          <div className="inline-block min-w-full align-middle">
+            <div className="overflow-hidden ">
+              <table className="min-w-full divide-y divide-slate-100 table-fixed dark:divide-slate-700">
+                <thead className=" border-t border-slate-100 dark:border-slate-800">
+                  <tr>
+                    {columns.map((column, i) => (
+                      <th key={i} scope="col" className=" table-th ">
+                        {column.label}
+                      </th>
+                    ))}
+                  </tr>
+                </thead>
+                <tbody className="bg-white divide-y divide-slate-100 dark:bg-slate-800 dark:divide-slate-700">
+                  {rows.map((row, i) => (
+                    <tr key={i}>
+                      <td className="table-td">{row.name}</td>
+                      <td className="table-td">{row.tickets}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
             </div>
-          </Card>
+          </div>
         </div>
-        <div className="2xl:col-span-4 lg:col-span-5 col-span-12">
-          <Card title="Statistic" headerslot={<SelectMonth />}>
-            <div className="grid md:grid-cols-2 grid-cols-1 gap-5">
-              <OrderChart />
-              <ProfitChart />
-              <div className="md:col-span-2">
-                <EarningChart />
-              </div>
-            </div>
-          </Card>
-        </div>
-        <div className="xl:col-span-6 col-span-12">
-          <Card title="Customer" headerslot={<SelectMonth />}>
-            <Customer />
-          </Card>
-        </div>
-        <div className="xl:col-span-6 col-span-12">
-          <Card title="Recent Orders" headerslot={<SelectMonth />} noborder>
-            <RecentOrderTable />
-          </Card>
-        </div>
-        <div className="xl:col-span-8 lg:col-span-7 col-span-12">
-          <Card title="Visitors Report" headerslot={<SelectMonth />}>
-            <BasicArea />
-          </Card>
-        </div>
-        <div className="xl:col-span-4 lg:col-span-5 col-span-12">
-          <Card title="Visitors By Gender" headerslot={<SelectMonth />}>
-            <VisitorRadar />
-          </Card>
-        </div>
-        <div className="xl:col-span-6 col-span-12">
-          <Card
-            title="Most Sales"
-            headerslot={
-              <div className="border border-slate-200 dark:border-slate-700 dark:bg-slate-900 rounded p-1 flex items-center">
-                <span
-                  className={` flex-1 text-sm font-normal px-3 py-1 transition-all duration-150 rounded cursor-pointer
-                ${
-                  filterMap === "global"
-                    ? "bg-slate-900 text-white dark:bg-slate-700 dark:text-slate-300"
-                    : "dark:text-slate-300"
-                }  
-                `}
-                  onClick={() => setFilterMap("global")}
-                >
-                  Global
-                </span>
-                <span
-                  className={` flex-1 text-sm font-normal px-3 py-1 rounded transition-all duration-150 cursor-pointer
-                  ${
-                    filterMap === "usa"
-                      ? "bg-slate-900 text-white dark:bg-slate-700 dark:text-slate-300"
-                      : "dark:text-slate-300"
-                  }
-              `}
-                  onClick={() => setFilterMap("usa")}
-                >
-                  USA
-                </span>
-              </div>
-            }
-          >
-            <MostSales2 filterMap={filterMap} />
-          </Card>
-        </div>
-        <div className="xl:col-span-6 col-span-12">
-          <Card title="Best selling products" headerslot={<SelectMonth />}>
-            <Products />
-          </Card>
-        </div>
-      </div> */}
-    </div>
+      </Card>
+    </>
   );
 };
 
-export default Ecommerce;
+export default StarterPage;
